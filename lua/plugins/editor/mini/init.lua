3cd5a5ecb5ee96597e9997fb7d576ba8688fb5b1
Robert Zhu <arcwandx86@gmail.com>
2024-06-06 00:32:07 -0700

    Style change

--- lua/plugins/editor/mini/init.lua
+++ lua/plugins/editor/mini/init.lua
@@ -2,15 +2,15 @@ return {
   { -- Collection of various small independent plugins/modules
     'echasnovski/mini.nvim',
     config = function()
-      require('mini.ai').setup(require('plugins.editor.mini.ai'))
-      -- require('mini.animate').setup(require('plugins.utility.mini.animate'))
-      require('mini.bracketed').setup(require('plugins.editor.mini.bracketed'))
-      require('mini.jump').setup(     require('plugins.editor.mini.jump'))
-      require('mini.jump2d').setup(   require('plugins.editor.mini.jump2d'))
-      require('mini.map').setup(      require('plugins.editor.mini.map'))
-      require('mini.move').setup(     require('plugins.editor.mini.move'))
-      -- require('mini.pairs').setup(    require('plugins.editor.mini.pairs'))
-      require('mini.surround').setup( require('plugins.editor.mini.surround'))
+      require('mini.ai').setup(       require 'plugins.editor.mini.ai')
+      -- require('mini.animate').setup(  require 'plugins.utility.mini.animate')
+      require('mini.bracketed').setup(require 'plugins.editor.mini.bracketed')
+      require('mini.jump').setup(     require 'plugins.editor.mini.jump')
+      require('mini.jump2d').setup(   require 'plugins.editor.mini.jump2d')
+      require('mini.map').setup(      require 'plugins.editor.mini.map')
+      require('mini.move').setup(     require 'plugins.editor.mini.move')
+      -- require('mini.pairs').setup(    require 'plugins.editor.mini.pairs')
+      require('mini.surround').setup( require 'plugins.editor.mini.surround')
     end,
   },
 }
