554d152f4e6c0d95b855e1a70a60203e63958fde
Robert Gu <arcwandx86@gmail.com>
2025-09-03 19:14:19 -0500

    Fix up keys

--- settings/keymaps.lua
+++ settings/keymaps.lua
@@ -34,16 +34,11 @@ map('i', '<Up>',   '<C-o>gk',                                                  '
 -- map('ni', '<C-A-k>', '<C-w><C-k>',                                             'Switch window UP')
 -- map('ni', '<C-A-l>', '<C-w><C-l>',                                             'Switch window RIGHT')
 map('n', '<A-i>', '<C-w>p',                                                    'Switch to most recent window')
-map('i', '<A-i>', '<Cmd>wincmd p<Cmd>',                                        'Switch to most recent window')
+map('i', '<A-i>', '<Cmd>wincmd p<CR>',                                         'Switch to most recent window')
 -- map('ni', '<C-A-S-h>', '<C-w><',                                               'Increase split width')
 -- map('ni', '<C-A-S-j>', '<C-w>+',                                               'Increase split height')
 -- map('ni', '<C-A-S-k>', '<C-w>-',                                               'Decrease split height')
 -- map('ni', '<C-A-S-l>', '<C-w>>',                                               'Decrease split width')
-
--- Buffers --------------------------------------------------------------------
--- Managed by mini.bracketed
--- map('n', ']b', '<Cmd>n<CR>',                                                   'Next buffer')
--- map('n', '[b', '<Cmd>N<CR>',                                                   'Prev buffer')
 
 -- [[ Editing ]] ==============================================================
 -- Single insertion/append/substitution/newline -------------------------------
@@ -51,15 +46,12 @@ map('n', '<leader>i', function() SingleInsert('i') end,                        '
 map('n', '<leader>a', function() SingleInsert('a') end,                        'Insert single char at start')
 map('n', '<leader>I', function() SingleInsert('I') end,                        'Append single char')
 map('n', '<leader>A', function() SingleInsert('A') end,                        'Append single char at end')
-map('v', '<leader>s', function() SingleInsert('s') end,                        'Substitute with single char')
-map('n', '<leader>o', [["m'".v:count."o<Esc>`'"]], { expr = true },            'Insert newline below')
-map('n', '<leader>O', [["m'".v:count."O<Esc>`'"]], { expr = true },            'Insert newline above')
 
 -- Drag selection -------------------------------------------------------------
 -- Managed by mini.move
 -- -- Vertical
-map('i', '<A-j>', function() vim.cmd.move('+'..vim.v.count1)   end,            'Drag line down')
-map('i', '<A-k>', function() vim.cmd.move('-'..vim.v.count1+1) end,            'Drag line up')
+-- map('i', '<A-j>', function() vim.cmd.move('+'..vim.v.count1)   end,            'Drag line down')
+-- map('i', '<A-k>', function() vim.cmd.move('-'..vim.v.count1+1) end,            'Drag line up')
 -- map('v', '<A-j>',  [[":m '>+".(v:count1)."<CR>gv"]], { expr = true },          'Drag block Down')
 -- map('v', '<A-k>',  [[":m '<-".(v:count1+1)."<CR>gv"]], { expr = true },        'Drag block up')
 map('nvi', '<A-Down>', '<A-j>', { remap = true },                              'Move selection down')
@@ -89,47 +81,6 @@ map('i', '<C-/>', '<C-o>gcc', { remap = true },                                '
 -- Replace word
 map('n', 'crn', '*Ncgn',                                                       'Replace word' )
 
-
--- [[ Fixes ]] ================================================================
--- Send deletions to 0 register instead of system clipboard -------------------
-map('nv', 'd', '"0d',                                                          'Delete')
-map('nv', 'D', '"0D',                                                          'Delete to end of line')
-map('nv', 'c', '"0c',                                                          'Change')
-map('nv', 'C', '"0C',                                                          'Change to end of line')
-map('nv', 's', '"0s',                                                          'Substitute')
-map('nv', 'S', '"0S',                                                          'Substitute line')
-map('nv', 'x', '"0x',                                                          'Delete character')
-map('nv', 'X', '"0X',                                                          'Delete character before')
--- Easy access to the 0 register and its contents
-map('', '0', '"0',                                                             'Send to 0 register')
-map('n', '<leader>0', '<Cmd>let @+=@0<CR>',                                    'Send 0 register to clipboard')
--- Send to other registers from clipboard
-local regs = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
-for i = 1, #regs do
-  local r = regs:sub(i, i)
-  map( 'n', '<leader>"' .. r, '<Cmd>let @' .. r .. '=@+<CR>',                  'Send clipboard to ' .. r .. ' register')
-end
--- Visual paste
--- map('x', 'p', 'pgv"0y:let @1=@+<CR>:let @+=@0<CR>:let @0=@1<CR>', { silent = true }, 'Paste')
-
--- Fix inconveniences caused by mapping H and L to BoL and EoL ----------------
-map('n', '^', '0',                                                             'Start of line')
--- Maintain H and L functionality since H and L are mapped
-map('n', 'M', '<Nop>',                                                         'Screen Pos')
-map('n', 'MH', 'H',                                                            'Top of page')
-map('n', 'MM', 'M',                                                            'Middle of page')
-map('n', 'ML', 'L',                                                            'Bottom of page')
-
--- Miscellaneous --------------------------------------------------------------
--- Visual indent
-map('v', '<', '<gv',                                                           'Un-indent')
-map('v', '>', '>gv',                                                           'Indent')
--- Total search count
-map('n', 'n', 'n<Cmd>lua print(SearchCount())<CR>')
-map('n', 'N', 'N<Cmd>lua print(SearchCount())<CR>')
--- Make ' jump horizontally not just vertically
-map('n', "'", '`')
-
 -- [[ Editor ]] ===============================================================
 -- Common shortcuts -----------------------------------------------------------
 -- Save
@@ -137,8 +88,8 @@ map('n',  '<C-s>',   '<Cmd>update<CR>',                                        '
 map('i',  '<C-s>',   '<Esc>:update<CR>',                                       'Save to file then exit insert mode')
 map('ni', '<C-A-s>', '<Cmd>wa<CR>',                                            'Save all buffers')
 -- Select all (not set in normal or visual mode because <C-a> is useful)
-map('i', '<C-a>', '<Esc>gggH<C-End>',                                          'Select entire buffer')
 map('o', '<C-a>', '<Cmd>normal! m9ggVG<CR><Cmd>normal! `9<CR>',                'Select entire buffer')
+map('v', 'gG', 'ggoG',                                                         'Select entire buffer')
 -- Shift to select
 map('i', '<S-Down>',  '<C-o>gh<Down>', { remap = true },                       'Down')
 map('i', '<S-Up>',    '<C-o>gh<Up>', { remap = true },                         'Up')
@@ -151,8 +102,7 @@ map('s', '<S-Right>', '<Right>',                                               '
 
 -- Convenience ----------------------------------------------------------------
 map('n', 'ZA', '<Cmd>wqa<CR>',                                                 'Write and close all')
-map('n', '<Esc>', '<Cmd>nohlsearch<CR>',                                       'Clear search highlight')
--- map('n', '<C-l>', '<Cmd>call setreg("/", [])<CR>',                          'Clear search register') -- By default, <C-l> clears the highlight
+map('n', 'Za', '<Cmd>qa!<CR>',                                                 'Close all')
 map('t', '<Esc>', '<C-\\><C-n>',                                               'Exit terminal mode') -- This won't work in all terminal emulators/tmux/etc.
 -- Mistake leniency
 map('no', 'gG', 'G',                                                           'Last line')
@@ -160,10 +110,40 @@ map('n', 'zZQ', 'ZQ',                                                          '
 map('n', 'zZZ', 'ZZ',                                                          'Write and close buffer')
 
 -- Toggle settings ------------------------------------------------------------
-map('n', '<leader>t', '<Nop>', 'Toggle')
-map('n', '<leader>tn', '<Cmd>setlocal relativenumber!<CR>',                     'Toggle relative numbering')
-map('n', '<leader>ts', '<Cmd>set spell!<CR>',                                   'Toggle spellcheck')
-map('n', '<leader>tw', '<Cmd>set wrap!<CR>',                                    'Toggle word wrapping')
-map('n', '<leader>t8', function() ToggleColorcolumn() end,                      'Toggle colorcolumn')
-map('n', '<leader>tr', function() ToggleAutoComment() end,                      'Toggle autocomment')
+map('n', '<leader>tn', '<Cmd>setlocal relativenumber!<CR>',                    'Toggle relative numbering')
+map('n', '<leader>ts', '<Cmd>set spell!<CR>',                                  'Toggle spellcheck')
+map('n', '<leader>tw', '<Cmd>set wrap!<CR>',                                   'Toggle word wrapping')
+map('n', '<leader>t<Tab>', '<Cmd>set list!<CR>',                               'Toggle whitespace rendering')
+map('n', '<leader>t8', function() ToggleColorcolumn() end,                     'Toggle colorcolumn')
+map('n', '<leader>tr', function() ToggleAutoComment() end,                     'Toggle autocomment')
 
+-- [[ Fixes ]] ================================================================
+-- Copy/paste with system clipboard
+local function SmartReg(key)
+  return (vim.v.register == '"' and '"+' or '') .. key
+end
+map('nx', 'y', function() return SmartReg('y') end, { expr = true },           'Yank')
+map('nx', 'Y', function() return SmartReg('y$') end, { expr = true },          'Yank to end')
+map('nx', 'p', function() return SmartReg('p') end, { expr = true },           'Put')
+map('nx', 'P', function() return SmartReg('P') end, { expr = true },           'Put before')
+-- Easy access vim registers from the clipboard
+map('n', '<leader>p', '<Cmd>let @+=@"<CR>',                                    'Send previous register to clipboard')
+
+-- Maintain H and L functionality since H and L are mapped
+map('n', 'MH', 'H',                                                            'Top of page')
+map('n', 'MM', 'M',                                                            'Middle of page')
+map('n', 'ML', 'L',                                                            'Bottom of page')
+
+-- Maintain ability to view function signature
+map('i', '<C-k>', vim.lsp.buf.signature_help,                                  'Function signature')
+-- Assumes that digraphs should be handled by the IME
+
+-- Miscellaneous --------------------------------------------------------------
+-- Visual indent
+map('v', '<', '<gv',                                                           'Un-indent')
+map('v', '>', '>gv',                                                           'Indent')
+-- Total search count
+map('n', 'n', 'n<Cmd>lua print(SearchCount())<CR>')
+map('n', 'N', 'N<Cmd>lua print(SearchCount())<CR>')
+-- Make ' jump horizontally not just vertically
+map('n', "'", '`')
